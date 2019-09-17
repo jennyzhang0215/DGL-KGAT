16f654907b437ce5c14df12591933b0949b1eb39
jennyzhang0215 <jennyzhang0215@gmail.com>
2019-09-17 13:28:22 +0800

    update

--- modules/dataset.py
+++ modules/dataset.py
@@ -11,36 +11,46 @@ class DataLoader(object):
 
         # ----------get number of entities and relations & then load kg data from kg_file into the DGLGraph------------.
         kg_file = os.path.join(data_dir, "kg_final.txt")
-        self.kg, relation_ids = self.load_kg2graph(kg_file)
+        self.kg_triples_np = self.load_kg2triplet(kg_file)
+
         train_file = os.path.join(data_dir, "train.txt")
-        self.rating_g = self.load_rating2graph(train_file)
-        #test_file = os.path.join(data_dir, "test.txt")
-        #self.test_data,  = self._load_rating2graph(test_file)
-        print(len(self.kg), len(self.rating_g), len(self.kg + self.rating_g))
-        self.all_g = dgl.hetero_from_relations(self.kg + self.rating_g)
-        self.all_g.edges["relation"].data['type'] = relation_ids
-        print("Data Statistic:\n\t#user:{}, #items:{}, #interactions:{}, #entities:{}, #relations:{}, #triplets:{}".format(
-            self.num_users, self.num_items, self.num_train, self.num_entities, self.num_relations, self.num_triples))
-        print("#users:", self.all_g.number_of_nodes('user'))
-        print("#entities:", self.all_g.number_of_nodes('entity'))
-        print("#interactions:", self.all_g.number_of_edges(('user', 'interact', 'entity')))
-        print("self.all_g['relation'].number_of_edges()", self.all_g['relation'].number_of_edges())
-        print("g.ntypes", self.all_g.ntypes)
-        print("g.etypes", self.all_g.etypes)
-        print("self.all_g['relation'].edata", self.all_g['relation'].edata)
-        print("metagraph", self.all_g.metagraph.edges())
+        train_user_id_np, train_item_id_np = self.load_rating(train_file)
+        ## remapping user ids after entities
+        self._user_mapping = {i: i+self.num_KG_entities for i in range(self.num_users)}
+        recsys_triplet = np.zeros((self.num_train*2, 3))
+        for i in range(self.num_train):
+            recsys_triplet[i, 0] = self._user_mapping[train_user_id_np[i]]
+            ### add the user2item interact relation
+            recsys_triplet[i, 1] = self.num_KG_relations
+            recsys_triplet[i, 2] = train_item_id_np[i]
+        for i in range(self.num_train, self.num_train*2):
+            recsys_triplet[i, 0] = train_item_id_np[i]
+            ### add the item2user interacted relation
+            recsys_triplet[i, 1] = self.num_KG_relations + 1
+            recsys_triplet[i, 2] = self._user_mapping[train_user_id_np[i]]
+        ### reverse the (head, relation, tail) direction, because we need tail --> head
+        all_triplet = np.vstack((self.kg_triples_np[:, [2,1,0]],  recsys_triplet))
+        g = dgl.DGLGraph()
+        g.add_nodes(self.num_all_entity)
+        g.add_edges(all_triplet[:, 0], all_triplet[:, 2])
+        self.g = g
+        self.rel_np = all_triplet[:, 1]
+
 
     @property
-    def num_entities(self):
-        return self._n_entities
+    def num_KG_entities(self):
+        return self._n_KG_entities
     @property
-    def num_relations(self):
-        return self._n_relations
+    def num_KG_relations(self):
+        return self._n_KG_relations
     @property
-    def num_triples(self):
-        return self._n_triples
+    def num_KG_triples(self):
+        return self._n_KG_triples
+    @property
+    def num_all_entity(self):
+        return self._n_KG_entities + self._n_users
 
-    def load_kg2graph(self, file_name):
+    def load_kg2triplet(self, file_name):
         """ Load the KG txt file into the 2-d numpy array
 
         Parameters
@@ -52,20 +62,15 @@ class DataLoader(object):
         np.array Shape:(num_triples, 3)
         """
         kg_triples_np = np.loadtxt(file_name, dtype=np.int32)
-        self.kg_triples_np = kg_triples_np
-        self._n_relations = np.unique(kg_triples_np[:, 1]).size
-        assert self._n_relations == kg_triples_np[:, 1].max() + 1
-        self._n_entities = np.unique(np.concatenate((kg_triples_np[:, 0], kg_triples_np[:, 2]))).size
-        assert self._n_entities == max(max(kg_triples_np[:, 0]), max(kg_triples_np[:, 2])) + 1
-        self._n_triples = kg_triples_np.shape[0]
+        ### check whether entity ids are continuous and start from 0
+        assert np.unique(kg_triples_np[:, 1]).size == max(kg_triples_np[:, 1]) + 1
+        assert np.unique(np.concatenate((kg_triples_np[:, 0], kg_triples_np[:, 2]))).size == \
+               max(max(kg_triples_np[:, 0]), max(kg_triples_np[:, 2])) + 1
+        self._n_KG_relations = np.unique(kg_triples_np[:, 1]).size
+        self._n_KG_entities = np.unique(np.concatenate((kg_triples_np[:, 0], kg_triples_np[:, 2]))).size
+        self._n_KG_triples = kg_triples_np.shape[0]
 
-        src = kg_triples_np[:, 0]
-        etype_id = kg_triples_np[:, 1]
-        dst = kg_triples_np[:, 2]
-        coo = sp.sparse.coo_matrix((np.ones(self.num_triples), (dst, src)),
-                                   shape=[self.num_entities, self.num_entities])
-        g = dgl.graph(coo, ntype='entity', etype='relation')
-        return [g], th.LongTensor(etype_id)
+        return kg_triples_np
 
     def create_KG_sampler(self, batch_size, neg_sample_size=1, mode=None, num_workers=5,
                           shuffle=True, exclude_positive=False):
@@ -76,33 +81,25 @@ class DataLoader(object):
         src = []
         dst = []
         lines = open(file_name, 'r').readlines()
-        user_l = []
-        item_l = []
         for l in lines:
             tmps = l.strip()
             inters = [int(i) for i in tmps.split(' ')]
             user_id, item_ids = inters[0], inters[1:]
             item_ids = list(set(item_ids))
-            user_l.append(user_id)
-            item_l += item_ids
             for i_id in item_ids:
                 src.append(user_id)
                 dst.append(i_id)
-        return src, dst, user_l, item_l
+        return np.array(src, dtype=np.int32), np.array(dst, dtype=np.int32)
 
-    def load_rating2graph(self, file_name):
-        src, dst, user_l, item_l = self._load_rating(file_name)
-        assert np.unique(user_l).size == len(user_l)
-        assert len(user_l) == max(user_l) + 1
-        self._n_users = np.unique(user_l).size
-        self._n_items = np.unique(item_l).size
-        assert self.num_items == max(item_l) + 1
+    def load_rating(self, file_name):
+        src, dst = self._load_rating(file_name)
+        ### check whether the user id / item id are continuous and starting from 0
+        assert np.unique(src).size == max(src) + 1
+        assert np.unique(dst).size == max(dst) + 1
+        self._n_users = np.unique(src).size
+        self._n_items = np.unique(dst).size
         self._n_train = len(src)
-        g = dgl.bipartite((src, dst), 'user', 'interact', 'entity',
-                          card=(self.num_users, self.num_entities))
-        rev_g = dgl.bipartite((dst, src), 'entity', 'interacted_by', 'user',
-                              card=(self.num_entities, self.num_users))
-        return [g, rev_g]
+        return src, dst
 
     @property
     def num_train(self):
