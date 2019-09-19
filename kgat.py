bfffc3a5d6e289a00f4006320ca0979f81643d3e
jennyzhang0215 <jennyzhang0215@gmail.com>
2019-09-19 15:35:08 +0800

    update

--- kgat.py
+++ kgat.py
@@ -2,39 +2,36 @@ import argparse
 
 from dataset import DataLoader
 from models import KGEModel, CFModel
-
 import torch as th
 import torch.optim as optim
+import utils
+import numpy as np
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Reproduce KGAT using DGL")
     parser.add_argument('--gpu', type=int, default=0, help='use GPU')
     ### Data parameters
     parser.add_argument('--data_name', nargs='?', default='yelp2018',  help='Choose a dataset from {yelp2018, last-fm, amazon-book}')
-    parser.add_argument('--pretrain', type=int, default=0, help='0: No pretrain, -1: Pretrain with the learned embeddings, 1:Pretrain with stored models.')
     parser.add_argument('--adj_type', nargs='?', default='si', help='Specify the type of the adjacency (laplacian) matrix from {bi, si}.')
-    parser.add_argument('--pbg', action='store_true', help='use pbg negative sampling.')
 
     ### Model parameters
-    parser.add_argument('--model_type', nargs='?', default='kgat', help='Specify a loss type from {kgat, bprmf, fm, nfm, cke, cfkg}.')
     parser.add_argument('--use_kge', type=bool, default=True, help='whether using knowledge graph embedding')
     parser.add_argument('--kge_size', type=int, default=32, help='KG Embedding size.')
     parser.add_argument('--embed_size', type=int, default=8, help='CF Embedding size.')
     parser.add_argument('--gnn_num_layer', type=int, default=2, help='the number of layers')
     parser.add_argument('--gnn_hidden_size', type=int, default=8, help='Output sizes of every layer')
-    parser.add_argument('--Ks', nargs='?', default='[20, 40, 60, 80, 100]', help='Output sizes of every layer')
     parser.add_argument('--dropout_rate', type=float, default=0.1, help='Keep probability w.r.t. node dropout (i.e., 1-dropout_ratio) for each deep layer. 1: no dropout.')
-    parser.add_argument('--use_att', type=bool, default=True, help='whether using attention mechanism')
     parser.add_argument('--train_kge', type=bool, default=False, help='Just for testing. Train KGE model')
     parser.add_argument('--regs', nargs='?', default='[1e-5,1e-5,1e-2]', help='Regularization for user and item embeddings.')
 
     ### Training parameters
     parser.add_argument('--max_iter', type=int, default=80000, help='train xx iterations')
     parser.add_argument("--grad_norm", type=float, default=1.0, help="norm to clip gradient to")
-    parser.add_argument('--evaluate_every', type=int, default=4096, help='the evaluation duration')
+    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate.')
     parser.add_argument('--batch_size', type=int, default=1024, help='CF batch size.')
     parser.add_argument('--batch_size_kg', type=int, default=4096, help='KG batch size.')
-    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate.')
+    parser.add_argument('--evaluate_every', type=int, default=1, help='the evaluation duration')
+    parser.add_argument("--eval-batch-size", type=int, default=500, help="batch size when evaluating")
     args = parser.parse_args()
 
     return args
@@ -59,39 +56,44 @@ def train(args):
     print("Dataset prepared ...")
     ### model
     ## n_entities, n_relations, entity_dim, relation_dim, num_gnn_layers, n_hidden, dropout, reg_lambda
-    cf_model = CFModel(n_entities=dataset.num_all_entities, n_relations=dataset.num_all_relations,
-                       entity_dim=args.embed_size, relation_dim=args.embed_size, num_gnn_layers=args.gnn_num_layer,
-                       n_hidden=args.gnn_hidden_size, dropout=args.dropout_rate, reg_lambda=0.01)
-    kge_model = KGEModel(n_entities=dataset.num_KG_entities, n_relations=dataset.num_KG_relations,
-                         entity_dim=args.embed_size, relation_dim=args.embed_size, reg_lambda=0.01)
+    if args.train_kge:
+        model = KGEModel(n_entities=dataset.num_KG_entities, n_relations=dataset.num_KG_relations,
+                             entity_dim=args.embed_size, relation_dim=args.embed_size, reg_lambda=0.01)
+    else:
+        model = CFModel(n_entities=dataset.num_all_entities, n_relations=dataset.num_all_relations,
+                           entity_dim=args.embed_size, relation_dim=args.embed_size, num_gnn_layers=args.gnn_num_layer,
+                           n_hidden=args.gnn_hidden_size, dropout=args.dropout_rate, reg_lambda=0.01)
     if use_cuda:
-        cf_model = cf_model.cuda()
-        kge_model = kge_model.cuda()
+        model = model.cuda()
     ### optimizer
-    cf_optimizer = optim.Adam(cf_model.parameters(), lr=args.lr)
-    kg_optimizer = optim.Adam(kge_model.parameters(), lr=args.lr)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
     print("Start training ...")
+    best_hit_score = 0.0
+    model_state_file = 'model_state.pth'
+
     for epoch in range(1, args.max_iter+1):
         if args.train_kge:
             iter = 0
             for h, r, pos_t, neg_t in kg_sampler:
                 iter +=1
-                kge_model.train()
+                model.train()
                 h_th = th.LongTensor(h)
                 r_th = th.LongTensor(r)
                 pos_t_th = th.LongTensor(pos_t)
                 neg_t_th = th.LongTensor(neg_t)
                 if use_cuda:
                     h_th, r_th, pos_t_th, neg_t_th = h_th.cuda(), r_th.cuda(), pos_t_th.cuda(), neg_t_th.cuda()
-                loss = kge_model(h_th, r_th, pos_t_th, neg_t_th)
+                loss = model(h_th, r_th, pos_t_th, neg_t_th)
                 # print("loss", loss)
                 loss.backward()
                 # print("start computing gradient ...")
-                kg_optimizer.step()
+                optimizer.step()
                 print("Epoch {:04d}, Iter {:04d} | Loss {:.4f} ".format(epoch, iter, loss.item()))
-                kg_optimizer.zero_grad()
+                optimizer.zero_grad()
         else:
-            cf_model.train()
+            model.train()
+            ### sample graph and sample user-item pairs
+            cf_sampler = dataset.CF_sampler(segment='train')
             user_ids, item_pos_ids, item_neg_ids = next(cf_sampler)
             user_ids_th = th.LongTensor(user_ids)
             item_pos_ids_th = th.LongTensor(item_pos_ids)
@@ -100,24 +102,37 @@ def train(args):
                 user_ids_th, item_pos_ids_th, item_neg_ids_th = \
                     user_ids_th.cuda(), item_pos_ids_th.cuda(), item_neg_ids_th.cuda()
 
-            embedding = cf_model(graph, th_n_id, th_e_type)
+            embedding = model(graph, th_n_id, th_e_type)
             print("embedding", embedding)
-            loss = cf_model.get_loss(embedding, user_ids_th, item_pos_ids_th, item_neg_ids_th)
+            loss = model.get_loss(embedding, user_ids_th, item_pos_ids_th, item_neg_ids_th)
             #print("loss", loss)
             loss.backward()
-            th.nn.utils.clip_grad_norm_(cf_model.parameters(), args.grad_norm)  # clip gradients
+            th.nn.utils.clip_grad_norm_(model.parameters(), args.grad_norm)  # clip gradients
 
             #print("start computing gradient ...")
-            cf_optimizer.step()
+            optimizer.step()
             print("Epoch {:04d} | Loss {:.4f} ".format(epoch, loss.item()))
-            cf_optimizer.zero_grad()
+            optimizer.zero_grad()
 
-        # if epoch % args.evaluate_every == 0:
-        #     if use_cuda:
-        #         model.cpu()
-        #
-        #     model.eval()
+        if epoch % args.evaluate_every == 0:
+            if use_cuda:
+                model.cpu()
+            model.eval()
+            cf_sampler = dataset.CF_sampler(segment='test')
+            test_hit_l = []
+            for test_user_ids, test_item_ids, _ in cf_sampler:
+                embedding = model(graph, th_n_id, th_e_type)
 
+                hit_rate = utils.calc_hit(embedding, (test_user_ids, test_item_ids), th.arange(dataset.num_items),
+                                      K=20, eval_bz=args.eval_batch_size)
+                test_hit_l.append(hit_rate)
+            hit_score = np.mean(test_hit_l)
+            # save best model
+            if hit_score > best_hit_score:
+                best_hit_score = hit_score
+                th.save({'state_dict': model.state_dict(), 'epoch': epoch}, model_state_file)
+            if use_cuda:
+                model.cuda()
 
 if __name__ == '__main__':
     args = parse_args()
