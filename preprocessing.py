b2b1c7117d178cc9bd1e34c3f21cd6379d326eb1
jennyzhang0215 <jennyzhang0215@gmail.com>
2019-09-19 17:45:11 +0800

    update

--- preprocessing.py
+++ preprocessing.py
@@ -11,17 +11,14 @@ kg_file = os.path.join(data_dir, "kg_final.txt")
 kg_pd = pd.read_csv(kg_file, sep=" ", names=['h', "r", "t"], engine='python')
 kg_pd = kg_pd.sort_values(by=['h'])
 print(kg_pd)
-kg_pd = kg_pd.sort_values(by=['t'])
-print(kg_pd)
-
 
 num_hop = 3
 item_ids = np.arange(45538).tolist()
-print("item_ids", len(item_ids))
 
 for i in range(num_hop):
+    print(i, "curr_items", len(item_ids))
     new_pd = kg_pd[kg_pd.h.isin(item_ids)]
-    print(new_pd)
+    print(new_pd.shape)
     item_ids= np.unique(np.concatenate((new_pd['h'].values, new_pd['t'].values)))
     print("item_ids",item_ids.size)
     item_ids = item_ids.tolist()
