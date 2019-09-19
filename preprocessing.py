295b37b8375bdb3ac4743d3406ac82803519a73c
jennyzhang0215 <jennyzhang0215@gmail.com>
2019-09-19 17:19:18 +0800

    update

--- preprocessing.py
+++ preprocessing.py
@@ -7,5 +7,7 @@ data_dir = os.path.realpath(os.path.join(os.path.abspath(__file__), '..', "datas
 kg_file = os.path.join(data_dir, "kg_final.txt")
 
 kg_pd = pd.read_csv(kg_file, sep=" ", names=['h', "r", "t"], engine='python')
+kg_pd = kg_pd.sort_values(by=['h'])
 print(kg_pd)
-
+kg_pd = kg_pd.sort_values(by=['t'])
+print(kg_pd)
