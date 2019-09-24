2bf1066251c0b957941b54fa87f42a8e5e297cb0
jennyzhang0215 <jennyzhang0215@gmail.com>
2019-09-25 00:11:34 +0800

    update

--- kgat.py
+++ kgat.py
@@ -15,14 +15,12 @@ def parse_args():
     parser.add_argument('--adj_type', nargs='?', default='si', help='Specify the type of the adjacency (laplacian) matrix from {bi, si}.')
 
     ### Model parameters
-    parser.add_argument('--use_kge', type=bool, default=True, help='whether using knowledge graph embedding')
-    parser.add_argument('--kge_size', type=int, default=64, help='KG Embedding size.')
     parser.add_argument('--entity_embed_dim', type=int, default=64, help='CF Embedding size.')
-    parser.add_argument('--relation_embed_dim', type=int, default=64, help='CF Embedding size.')
+    parser.add_argument('--relation_embed_dim', type=int, default=32, help='CF Embedding size.')
     parser.add_argument('--gnn_num_layer', type=int, default=2, help='the number of layers')
     parser.add_argument('--gnn_hidden_size', type=int, default=64, help='Output sizes of every layer')
     parser.add_argument('--dropout_rate', type=float, default=0.1, help='Keep probability w.r.t. node dropout (i.e., 1-dropout_ratio) for each deep layer. 1: no dropout.')
-    parser.add_argument('--regs', type=float, default=0.00001, help='Regularization for user and item embeddings.')
+    parser.add_argument('--regs', type=float, default=0.001, help='Regularization for user and item embeddings.')
 
     ### Training parameters
     parser.add_argument('--train_kge', type=bool, default=True, help='Just for testing. Train KGE model')
