f8cc820765c593fd6b4bdfee034ae3e63cfb9020
jennyzhang0215 <jennyzhang0215@gmail.com>
2019-10-07 00:43:27 -0700

    update

--- kgat.py
+++ kgat.py
@@ -31,7 +31,7 @@ def parse_args():
     parser.add_argument('--max_epoch', type=int, default=100, help='train xx iterations')
     parser.add_argument("--grad_norm", type=float, default=1.0, help="norm to clip gradient to")
     parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate.')
-    parser.add_argument('--batch_size', type=int, default=1024, help='CF batch size.')
+    parser.add_argument('--batch_size', type=int, default=10000, help='CF batch size.')
     parser.add_argument('--batch_size_kg', type=int, default=2048, help='KG batch size.')
     parser.add_argument('--evaluate_every', type=int, default=1, help='the evaluation duration')
     parser.add_argument('--print_kg_every', type=int, default=100, help='the print duration')
