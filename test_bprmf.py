751fbec1ae59767a6ce0fca7babe80790f451e85
jennyzhang0215 <jennyzhang0215@gmail.com>
2019-11-04 15:52:47 +0800

    activate gpu

--- test_bprmf.py
+++ test_bprmf.py
@@ -24,24 +24,29 @@ def parse_args():
     args.save_id = creat_log_id(args.save_dir)
     return args
 
-def eval(all_embedding, train_user_dict, eval_user_dict, item_id_range):
+def eval(all_embedding, train_user_dict, eval_user_dict, item_id_range, use_cuda):
     with th.no_grad():
         recall, ndcg = metric.calc_recall_ndcg(all_embedding, train_user_dict, eval_user_dict,
-                                               item_id_range, K=20)
+                                               item_id_range, K=20, use_cuda=use_cuda)
     return recall, ndcg
 
 if __name__ == '__main__':
     args = parse_args()
     logging_config(folder=args.save_dir, name='log{:d}'.format(args.save_id), no_console=False)
+    ### check context
+    use_cuda = args.gpu >= 0 and th.cuda.is_available()
+    if use_cuda:
+        th.cuda.set_device(args.gpu)
 
     dataset = DataLoader(args.data_name, use_KG=False, use_pretrain=args.use_pretrain, seed=args.seed)
     assert dataset.num_items + dataset.num_users == dataset.num_all_nodes
+
     item_id_range = th.arange(dataset.num_items, dtype=th.long)
-
     embeds = th.tensor(np.vstack((dataset.item_pre_embed, dataset.user_pre_embed)))
-
-    val_recall, val_ndcg = eval(embeds, dataset.train_user_dict, dataset.valid_user_dict, item_id_range)
-    test_recall, test_ndcg = eval(embeds, dataset.train_user_dict, dataset.test_user_dict, item_id_range)
+    if use_cuda:
+        item_id_range, embeds = item_id_range.cuda(), embeds.cuda()
+    val_recall, val_ndcg = eval(embeds, dataset.train_user_dict, dataset.valid_user_dict, item_id_range, use_cuda)
+    test_recall, test_ndcg = eval(embeds, dataset.train_user_dict, dataset.test_user_dict, item_id_range, use_cuda)
 
     logging.info("Test recall: {:.5f}, ndcg:{:.5f}\t\tvalid recall: {:.5f}, ndcg:{:.5f}".format(
         test_recall, test_ndcg, val_recall, val_ndcg))
